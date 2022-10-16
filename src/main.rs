2a15cd12ac44b932121a69884412a850997b9072
Akash R <akash.r@thehindu.co.in>
2022-10-16 21:27:14 +0530

    Added comments and procedure to be done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-}
+/**
+ * **************** Expense Tracker in Rust ******************
+ * This is a simple project to track your expenses and save it
+ * to a file. This is just beginning, which could grow to many
+ * features as I get familiar with rust. The following are the
+ * initial procedures to have a working skeleton of this game:
+ *
+ * 1. Take user input for either expense or income.
+ * 2. Get amount input.
+ * 3. Do the necessary manipulation (add / less).
+ * 4. Save the transaction.
+ * 5. Show the available balance.
+ * 6. Exit.
+ *
+ * The above are the procedures to be followed to get a working
+ * skeleton of the game.
+ */
+fn main() {}
